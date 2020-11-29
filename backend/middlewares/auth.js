31b2f9e17cdbdb3a2e9e000cdf92301d5cd39337
kluk42 <nikitanemykin@mail.ru>
2020-11-29 21:43:42 +0300

    Хардкод токена

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const WrongRequestErr = require('../errors/wrong-request-err');
 
 module.exports = (req, res, next) => {
-    const { JWT_SECRET } = process.env;
+    const JWT_SECRET = '6161330a563a2857b56b2aa9773132b13955c9d8520992a9489fd8be5ae0d720';
     const { authorization } = req.headers;
     const token = authorization.replace('Bearer ', '');
     let payload = '';
