d6eea8a9b4bc115002242cfc6b7f1487de0fe73e
kluk42 <nikitanemykin@mail.ru>
2020-11-29 21:53:58 +0300

    Первая итерация

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -9,6 +9,7 @@ const {
 } = require('../controllers/users');
 const auth = require('../middlewares/auth');
 const requestValidation = require('../middlewares/request-validation');
+const app = require('../app');
 
 router.get('/users', requestValidation, auth, getUsers);
 
@@ -17,6 +18,12 @@ router.get('/users/me', requestValidation, auth, getUser);
 router.patch('/users/me', requestValidation, auth, updateUser);
 
 router.patch('/users/me/avatar', requestValidation, auth, updateAvatar);
+
+app.get('/crash-test', () => {
+    setTimeout(() => {
+        throw new Error('Сервер сейчас упадёт');
+    }, 0);
+});
 
 router.post('/users/signin', login);
 
