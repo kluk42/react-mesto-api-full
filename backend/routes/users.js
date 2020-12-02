bd66cbe5dec71600bb396d3373b5deaf2770f80f
kluk42 <nikitanemykin@mail.ru>
2020-12-02 08:52:28 +0300

    Вторая итерация

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -31,11 +31,8 @@ router.get('/crash-test', () => {
 router.post('/signin', celebrate({
     body: Joi.object().keys({
         email: Joi.string().required().email(),
-        password: Joi.string().required().min(8),
+        password: Joi.string().required().min(6),
     }).unknown(true),
-    headers: Joi.object().keys({
-        authorization: Joi.string().required(),
-    }),
 }), login);
 
 router.post('/signup', celebrate({
